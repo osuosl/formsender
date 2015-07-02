8aea58e7793a9ca6c4f81a6bcfcca8578c43e1f3
matthewrsj <mrsj@osuosl.com>
2015-07-02 10:21:11 -0700

    Added tests to werkzeug_test.py
    ref #2
    Added:
     - test_create_msg_no_content
     - test_create_msg_no_content_get_method

--- request_handler.py
+++ request_handler.py
@@ -75,6 +75,7 @@ def create_msg(request):
         if message:
             return message
         return None
+    return None
 
 
 # Application logic
--- werkzeug_test.py
+++ werkzeug_test.py
@@ -9,7 +9,7 @@ from StringIO import StringIO
 
 class Test_formsender(unittest.TestCase):
 
-    def test_create_msg(self):
+    def test_create_msg_with_content(self):
         builder = EnvironBuilder(method='POST',
                                  data={'foo' : 'this is some text',
                                       'file': 'my file contents',
@@ -19,6 +19,18 @@ class Test_formsender(unittest.TestCase):
         assert (create_msg(request)['foo'] == builder.form['foo'] and
                create_msg(request)['file'] == builder.form['file'] and
                create_msg(request)['test'] == builder.form['test'])
+
+    def test_create_msg_no_content(self):
+        builder = EnvironBuilder(method='POST', data={})
+        env = builder.get_environ()
+        request = Request(env)
+        assert create_msg(request) is None
+
+    def test_create_msg_no_content_get_method(self):
+        builder = EnvironBuilder(method='GET', data={})
+        env = builder.get_environ()
+        request = Request(env)
+        assert create_msg(request) is None
 
     def test_send_email(self):
         form = Forms()
