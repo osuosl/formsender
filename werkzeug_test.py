175c7f78ec950048de5282d6b99c44905c43cdab
matthewrsj <mrsj@osuosl.com>
2015-07-02 09:26:04 -0700

    Added create_msg method and test
    ref #2

--- werkzeug_test.py
+++ werkzeug_test.py
@@ -1,3 +1,25 @@
-from werkzueg.test import Client
-from werkzueg.testapp import test_app
-from werkzeug.wrappers import BaseResponse
+import unittest
+from request_handler import Forms, create_msg
+from werkzeug.test import Client
+from werkzeug.testapp import test_app
+from werkzeug.wrappers import BaseResponse, Request
+from werkzeug.test import EnvironBuilder
+from StringIO import StringIO
+
+
+class Test_formsender(unittest.TestCase):
+
+    def test_create_msg(self):
+        builder = EnvironBuilder(method='POST',
+                                 data={'foo' : 'this is some text',
+                                      'file': 'my file contents',
+                                      'test': 'test.txt'})
+        env = builder.get_environ()
+        request = Request(env)
+        assert (create_msg(request)['foo'] == builder.form['foo'] and
+               create_msg(request)['file'] == builder.form['file'] and
+               create_msg(request)['test'] == builder.form['test'])
+
+
+if __name__ == '__main__':
+    unittest.main()
