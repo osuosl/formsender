8480d47be5e76bb5bee74786698f8bb2fc6fb289
MaraJade <catbookgirl@gmail.com>
2015-07-02 11:15:53 -0700

    Started creating tests

--- werkzeug_test.py
+++ werkzeug_test.py
@@ -1,3 +1,17 @@
 from werkzueg.test import Client
 from werkzueg.testapp import test_app
-from werkzeug.wrappers import BaseResponse
+from werkzeug.wrappers import BaseResponse, Request
+from werkzeug.test import EnvironBuilder
+
+from request_handler import *
+
+class FormsTestCase():
+    def test_things(self):
+        builder = EnvironBuilder(method='POST', data={'age': '4', 'name':
+            'banana'})
+        env = builder.get_environ()
+
+        req = Request(env)
+
+if __name__ == '__main__':
+#do things
