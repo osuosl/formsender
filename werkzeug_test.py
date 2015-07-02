a1e2fd502d90d22e33b820b3166cb12fe8a30b20
matthewrsj <mrsj@osuosl.com>
2015-07-02 16:30:42 -0700

    Commented test cases and updated test
    Added comments to describe tests
    Renamed test class with camelcase
    Fixed test_create_msg_no_content_get_method to include content
        since create_msg with no content already returns None

--- werkzeug_test.py
+++ werkzeug_test.py
@@ -7,9 +7,14 @@ from werkzeug.test import EnvironBuilder
 from StringIO import StringIO
 
 
-class Test_formsender(unittest.TestCase):
+class TestFormsender(unittest.TestCase):
 
     def test_create_msg_with_content(self):
+        """
+        Tests create_msg with content in the POST request
+
+        Checks that each element in the request is returned create_msg
+        """
         builder = EnvironBuilder(method='POST',
                                  data={'foo' : 'this is some text',
                                       'file': 'my file contents',
@@ -21,18 +26,37 @@ class Test_formsender(unittest.TestCase):
                create_msg(req)['test'] == builder.form['test'])
 
     def test_create_msg_no_content(self):
+        """
+        Tests create_msg with no content in the POST request
+
+        Checks that create_msg returns None
+        """
         builder = EnvironBuilder(method='POST', data={})
         env = builder.get_environ()
         req = Request(env)
         assert create_msg(req) is None
 
-    def test_create_msg_no_content_get_method(self):
-        builder = EnvironBuilder(method='GET', data={})
+    def test_create_msg_with_content_get_method(self):
+        """
+        Tests create_msg with content in a GET request
+
+        Checks that create_msg returns None
+        """
+        builder = EnvironBuilder(method='GET',
+                                 data={'foo' : 'this is some text',
+                                      'file': 'my file contents',
+                                      'test': 'test.txt'})
         env = builder.get_environ()
         req = Request(env)
         assert create_msg(req) is None
 
     def test_send_email(self):
+        """
+        Tests send_email
+
+        send_email returns True when it successfully sends an email and
+        False when unsuccessfull.
+        """
         app = Forms()
         assert app.send_email({'age': u'test', 'name': u'dict'})
 
