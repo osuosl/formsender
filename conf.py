685dd2996ae1513490e9b939cf29c3d3ec1a55a5
matthewrsj <mrsj@osuosl.com>
2015-08-04 16:38:14 -0700

    Removed personal email from conf

--- conf.py
+++ conf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-EMAIL = u'mrsj@osuosl.org'
+EMAIL = u'email@example.com'
 TOKN = u'1234567890-='
 CEILING = 10
 DUPLICATE_CHECK_TIME = 3600  # seconds -- 60 seconds * 60 minutes
