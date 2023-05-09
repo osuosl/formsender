05b54e1766a56ff52d5e42c18c3e4fc4fc3db500
Lance Albertson <lance@osuosl.org>
2023-05-09 16:34:44 -0700

    Setup forwarded for
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ exec gunicorn \
   --access-logfile '-' \
   --error-logfile '-' \
   --log-file "-" \
+  --forwarded-allow-ips="140.211.9.50,140.211.9.52,140.211.9.53" \
   -w 4 \
   -b 0.0.0.0:5000 \
   'request_handler:create_app()'
