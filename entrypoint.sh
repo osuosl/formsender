aae84c2ebf4554b6ad0c1237539b24833cd15ed5
Lance Albertson <lance@osuosl.org>
2023-05-09 16:39:42 -0700

    Update log format
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ exec gunicorn \
   --access-logfile '-' \
   --error-logfile '-' \
   --log-file "-" \
+  --access-logformat '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' \
   --forwarded-allow-ips="140.211.9.50,140.211.9.52,140.211.9.53" \
   -w 4 \
   -b 0.0.0.0:5000 \
